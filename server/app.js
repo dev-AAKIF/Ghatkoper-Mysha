d5f37b9f205eebdf31072d12efb23c7c719b534b
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-08-08 13:18:24 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,8 @@ app.use('/api/v1/portfolio',portfolioRoutes)
 
 app.use(
   cors({
-    origin:process.env.ORIGIN    ,
+    // origin:process.env.ORIGIN    ,
+    origin: "https://ghatkoper-mysha-client.onrender.com",
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], 
 
